7bdcd87d705182a947aacd9c7c26fff36afa9413
UJ2402 <ujgupta24@gmail.com>
2023-07-03 16:29:08 +0530

    firebase working

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { auth } from "./firebaseApp";
+import { auth } from "./firebaseApp";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout, selectUser } from "./features/userSlice";
 import ProfileScreen from "./screens/ProfileScreen";
@@ -11,23 +11,23 @@ import ProfileScreen from "./screens/ProfileScreen";
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch()
-  // useEffect(() => {
-  //   const unsubscribe = auth.onAuthStateChanged((userAuth) => {
-  //     if (userAuth) {
-  //       dispatch(
-  //         login({
-  //           uid: userAuth.uid,
-  //           email: userAuth.email,
-  //         })
-  //       );
-  //     } else {
-  //       //Logged out
-  //       dispatch(logout());
-  //     }
-  //   });
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        //Logged out
+        dispatch(logout());
+      }
+    });
 
-  //   return unsubscribe;
-  // }, [dispatch]);
+    return unsubscribe;
+  }, [dispatch]);
 
   return (
     <div className="app">
--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,19 +1,20 @@
-// import { initializeApp } from "@firebase/app";
-// import "@firebase/auth";
-// import "@firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// import {getStorage} from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDCB_8fwvmWuAtiNjC4tQ4ZHJw6rgCfbew",
-//   authDomain: "netflix-clone-98159.firebaseapp.com",
-//   projectId: "netflix-clone-98159",
-//   storageBucket: "netflix-clone-98159.appspot.com",
-//   messagingSenderId: "488494940109",
-//   appId: "1:488494940109:web:9d3b18fd908f9a38bfce84",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDCB_8fwvmWuAtiNjC4tQ4ZHJw6rgCfbew",
+  authDomain: "netflix-clone-98159.firebaseapp.com",
+  projectId: "netflix-clone-98159",
+  storageBucket: "netflix-clone-98159.appspot.com",
+  messagingSenderId: "488494940109",
+  appId: "1:488494940109:web:9d3b18fd908f9a38bfce84",
+};
 
-// const firebaseApp = initalizeApp(firebaseConfig);
-// const db = firebaseApp.firestore();
-// const auth = firebase.auth();
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
 
-// export { auth };
-// export default db;
+export { auth };
+export default db;
